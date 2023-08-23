aa6b9860488d304c58cc669da44f56cb6790bac1
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-23 23:11:31 +0500

    Delete api directory

--- api/routes/contact.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {postContact, getContacts, selectedDeleteContact} from '../controllers/contact.controller.js'
-
-const router = express.Router();
-
-router.post("/post", postContact);
-router.get("/get", getContacts);
-router.delete("/delete/:ids", selectedDeleteContact);
-export default router;
--- api/routes/mail.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendMail } from "../controllers//mail.controller.js";
-import { verifyToken } from "../middleware/jwt.js";
-const router = express.Router();
-
-router.post("/sendMail",verifyToken, sendMail)
-
-export default router;
