eeada827b03f5274721da15b45caefd2f566963a
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-24 10:37:24 +0500

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://shampy-marketplace.vercel.app/api/",
+  baseURL: "http://localhost:8800/api/",
   withCredentials: true,
 });
 
