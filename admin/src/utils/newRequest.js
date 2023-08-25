be9737cd9b87b2e82fbd380943edfaeeba95205a
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-25 15:05:12 +0500

    Update newRequest.js

--- admin/src/utils/newRequest.js
+++ admin/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://shampy-api.cyclic.cloud/api/",
   withCredentials: true,
 });
 
