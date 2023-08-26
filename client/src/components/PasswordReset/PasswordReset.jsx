788d6f23321598d6aa5909d31260be6ac5f7b89f
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-26 10:54:20 +0500

    Update PasswordReset.jsx

--- client/src/components/PasswordReset/PasswordReset.jsx
+++ client/src/components/PasswordReset/PasswordReset.jsx
@@ -13,7 +13,7 @@ const PasswordReset = () => {
 	const [msg, setMsg] = useState("");
 	const [error, setError] = useState("");
 	const param = useParams();
-	const url = `https://shampy-api.cyclic.cloud/api/auth/${param.id}/${param.token}`;
+	const url = `https://shampy.cyclic.cloud/api/auth/${param.id}/${param.token}`;
 
     const initialValues = {
     newPassword: '',
