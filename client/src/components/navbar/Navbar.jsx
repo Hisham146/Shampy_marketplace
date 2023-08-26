bad833b11c0d9915e80d0e7070d5b6c328a15eec
Hisham146 <99106513+Hisham146@users.noreply.github.com>
2023-08-26 12:13:53 +0500

    Update Navbar.jsx

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -185,9 +185,6 @@ export default function Navbar({ onLogout }) {
               </button>  </NavLink>
             </>
           )}
-              
-           
-
             </div>
 
           </div>
