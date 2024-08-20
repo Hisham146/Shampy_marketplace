97cc5fbe25c4c705d31241d409b0b7d5c03da4cb
Hisham Ahmed <99106513+Hisham146@users.noreply.github.com>
2024-08-20 19:10:50 +0400

    Update newRequest.js

--- admin/src/utils/newRequest.js
+++ admin/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "https://shampy.cyclic.cloud/api/",
-  withCredentials: true,
+  baseURL: "https://shampyapi-production.up.railway.app/api/",
+  withCredentials: false,
 });
 
 export default newRequest;
